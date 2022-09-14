71dfd9d2e74c72daceae3e1b0ed8c15eb5905281
Ararchy01 <ararchy0621@gmail.com>
2022-09-13 17:09:57 -0700

    Modify: Article build error avoid

--- pages/components/article/index.tsx
+++ pages/components/article/index.tsx
@@ -13,36 +13,40 @@ const Article = ({ articles, title }) => {
           </h1>
         )}
       </div>
-      {articles.map((article, index) => {
-        const time = moment(article.publishedAt || moment.now())
-          .fromNow()
-          .slice(0, 1);
-        return (
-          <a href={article.url} key={index} target="_blank" rel="noopener">
-            <article className={styles.article__main}>
-              <div className={styles.article__title}>
-                <p>{article.title}</p>
-                <p className={styles.article__time}>{time}h ago</p>
-              </div>
-              {article.urlToImage ? (
-                <img
-                  key={index}
-                  src={article.urlToImage}
-                  className={styles.article__img}
-                  alt={`${article.title} image`}
-                />
-              ) : (
-                <img
-                  key={index}
-                  src={`/youtube.png`}
-                  className={styles.article__img}
-                  alt={`${article.title} image`}
-                />
-              )}
-            </article>
-          </a>
-        );
-      })}
+      {articles ? (
+        articles.map((article, index) => {
+          const time = moment(article.publishedAt || moment.now())
+            .fromNow()
+            .slice(0, 1);
+          return (
+            <a href={article.url} key={index} target="_blank" rel="noopener">
+              <article className={styles.article__main}>
+                <div className={styles.article__title}>
+                  <p>{article.title}</p>
+                  <p className={styles.article__time}>{time}h ago</p>
+                </div>
+                {article.urlToImage ? (
+                  <img
+                    key={index}
+                    src={article.urlToImage}
+                    className={styles.article__img}
+                    alt={`${article.title} image`}
+                  />
+                ) : (
+                  <img
+                    key={index}
+                    src={`/youtube.png`}
+                    className={styles.article__img}
+                    alt={`${article.title} image`}
+                  />
+                )}
+              </article>
+            </a>
+          );
+        })
+      ) : (
+        <></>
+      )}
     </section>
   );
 };
